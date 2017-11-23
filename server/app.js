6af1cc5067911dcefd287dd1ede3666f18d751af
Adesile Emmanuel <emma2adesile@gmail.com>
2017-11-23 17:58:43 +0100

    Create .babelrc and .eslint files

---
+++ server/app.js
@@ -0,0 +1,14 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+
+const app = express();
+
+
+app.use(bodyParser.json());
+
+
+app.get('/', (req, res) => {
+  res.send('Server is up and running')
+})
+
+app.listen(9000);
