6e1ea3eb7f7edf055da0108b0f0bdf7cceb1d659
Adesile Emmanuel <emma2adesile@gmail.com>
2017-11-24 11:41:18 +0100

    Edit events model

--- server/model/events.js
+++ server/model/events.js
@@ -4,35 +4,35 @@ const events = [
       name:"Valedictory",
       center: "Balmoral",
       facilities:"Projector, Stage lights",
-      Booked_Date:"12/12/2017"
+      date:"12/12/2017"
   },
   {
       id:2,
       name:"Wedding Ceremony",
       center: "Faith Plaza",
       facilities:"Projector, Stage lights",
-      Booked_Date:"12/12/2017"
+      date:"12/12/2017"
   },
   {
       id:3,
       name:"Get together",
       center: "Five Points",
       facilities:"Projector, Stage lights",
-      Booked_Date:"12/12/2017"
+      date:"12/12/2017"
   },
   {
       id:4,
       name:"Seminar",
       center: "Balmoral",
       facilities:"Projector, Stage lights",
-      Booked_Date:"12/12/2017"
+      date:"12/12/2017"
   },
   {
       id:5,
       name:"Conference",
       center: "Radisson Blu",
       facilities:"Projector, Stage lights",
-      Booked_Date:"12/12/2017"
+      date:"12/12/2017"
   }
   
 ];
