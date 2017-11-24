8235cc968e8f2545d93bf03d3b5407c1336e0a63
Adesile Emmanuel <emma2adesile@gmail.com>
2017-11-24 11:57:04 +0100

    Edit centers model

---
+++ server/model/centers.js
@@ -0,0 +1,36 @@
+const centers = [
+  {
+      id: 1,
+      name:"Baywatch Events Center",
+      location: "13, Sanusi Fafunwa, VI, Lagos",
+      facilities:"Projector, Stage lights",
+  },
+  {
+      id: 2,
+      name:"Boldview Event center",
+      location: "20, Cali road, Ikorodu, Lagos",
+      facilities:"Projector, Stage lights",
+  },
+  {
+      id: 3,
+      name: "CrownTop Event center",
+      location: "34, Cold road, Ikeja, Lagos",
+      facilities:"Projector, Stage lights, Sound-production",
+  },
+  {
+      id: 4,
+      name:"The Jewel Center",
+      location: "3, Dark alley, Ikoyi, Lagos",
+      facilities:"Projector, Stage lights",
+  },
+  {
+      id: 5,
+      name: "OpenArms",
+      location: "29, Salvation road, Opebi, Lagos",
+      facilities:"Projector, Stage lights, Gallery, Gallety",
+  }
+  
+];
+
+export default centers;
+
